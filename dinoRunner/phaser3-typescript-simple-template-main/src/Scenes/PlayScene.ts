b92a8d0a662f98777af9be115ab67c2bfa7f9d99
ecorey <bengstrom32@gmail.com>
2024-06-10 12:34:56 -0400

    commit

--- dinoRunner/phaser3-typescript-simple-template-main/src/Scenes/PlayScene.ts
+++ dinoRunner/phaser3-typescript-simple-template-main/src/Scenes/PlayScene.ts
@@ -6,7 +6,7 @@ class PlayScene extends Phaser.Scene {
 
 
     player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-
+    startTrigger: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
     get gameHeight() {
         return this.game.config.height as number;
@@ -20,9 +20,22 @@ class PlayScene extends Phaser.Scene {
     
 
     create() {
+        
         this.createEnvironment();
         this.createPlayer();
+
+        this.startTrigger = this.physics.add.sprite(0, 10, null)
+        .setAlpha(0)
+        .setOrigin(0, 1);
+
         this.registerPlayerControl();
+
+
+        this.physics.add.overlap(this.startTrigger, this.player, () => {
+            console.log('Collision detected!');
+        })
+
+
     }
 
 
@@ -33,10 +46,12 @@ class PlayScene extends Phaser.Scene {
 
 
     createPlayer() {
+
         this.player = this.physics.add.sprite(0, this.gameHeight, 'dino-idle')
             .setOrigin(0, 1);
 
-        this.player.setGravityY(5000)
+        this.player
+            .setGravityY(5000)
             .setCollideWorldBounds(true)
             .setBodySize(44, 92);
 
